ba3d8276178c381465299b9f25d68819ed8b97ad
AkoDivina <divinako963@gmail.com>
2024-04-21 18:08:08 +0100

    everything

---
+++ includes/footer.php
@@ -0,0 +1,94 @@
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="mb-5">
+                    <h3 class="footer-heading mb-4">About DreamHome</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe pariatur reprehenderit
+                        vero
+                        atque, consequatur id ratione, et non dignissimos culpa? Ut veritatis, quos illum totam
+                        quis
+                        blanditiis, minima minus odio!</p>
+                </div>
+
+
+
+            </div>
+            <div class="col-lg-4 mb-5 mb-lg-0">
+                <div class="row mb-5">
+                    <div class="col-md-12">
+                        <h3 class="footer-heading mb-4">Navigations</h3>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <ul class="list-unstyled">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Buy</a></li>
+                            <li><a href="#">Rent</a></li>
+                            <li><a href="#">Properties</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <ul class="list-unstyled">
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">Terms</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+
+            </div>
+
+            <div class="col-lg-4 mb-5 mb-lg-0">
+                <h3 class="footer-heading mb-4">Follow Us</h3>
+
+                <div>
+                    <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+                    <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+                    <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+                    <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+                </div>
+
+
+
+            </div>
+
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+            <div class="col-md-12">
+                <p>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;<script data-cfasync="false"
+                        src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+                    <script>
+                    // document.write(new Date().getFullYear());
+                    </script> Some inputs are empty. Go back and check<i class="icon-heart text-danger"
+                        aria-hidden="true"></i>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </p>
+            </div>
+
+        </div>
+    </div>
+</footer>
+
+<script src="<?php echo APPURL; ?>/js/jquery-3.3.1.min.js"></script>
+<script src="<?php echo APPURL; ?>/js/jquery-migrate-3.0.1.min.js"></script>
+<script src="<?php echo APPURL; ?>/js/jquery-ui.js"></script>
+<script src="<?php echo APPURL; ?>/js/popper.min.js"></script>
+<script src="<?php echo APPURL; ?>/js/bootstrap.min.js"></script>
+<script src="<?php echo APPURL; ?>/js/owl.carousel.min.js"></script>
+<script src="<?php echo APPURL; ?>/js/mediaelement-and-player.min.js"></script>
+<script src="<?php echo APPURL; ?>/js/jquery.stellar.min.js"></script>
+<script src="<?php echo APPURL; ?>/js/jquery.countdown.min.js"></script>
+<script src="<?php echo APPURL; ?>/js/jquery.magnific-popup.min.js"></script>
+<script src="<?php echo APPURL; ?>/js/bootstrap-datepicker.min.js"></script>
+<script src="<?php echo APPURL; ?>/js/aos.js"></script>
+
+<script src="<?php echo APPURL; ?>/js/main.js"></script>
+
+
+</div>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+define("APPURL", "http://localhost:3000");
+
+?>
+
+<head>
+    <title>DreamHome &mdash; Colorlib Website Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Roboto+Mono:300,400,500">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/magnific-popup.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/jquery-ui.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/mediaelementplayer.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/animate.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/fl-bigmug-line.css">
+
+
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/aos.css">
+
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/style.css">
+
+</head>
+
+<body>
